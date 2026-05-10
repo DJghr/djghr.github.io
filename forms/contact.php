e42f827fe4990b6b859920115513dcd2efaaee9b
DJghr <djebril.gheribi.pro@gmail.com>
2026-05-10 20:06:46 +0200

    Mail config

--- forms/contact.php
+++ forms/contact.php
@@ -7,7 +7,7 @@
   */
 
   // Replace contact@example.com with your real receiving email address
-  $receiving_email_address = 'djebril.gheribi.pro@gmail.com';
+  $receiving_email_address = '455f0c22267abdbdb7d2d96a8d6f88f7';
 
   if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
     include( $php_email_form );
